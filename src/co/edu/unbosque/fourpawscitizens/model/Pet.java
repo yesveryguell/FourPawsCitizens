25e2613742e3fadcb8a4260dc6979f4318ada454
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-02-19 14:52:14 -0500

    Ricardo buenas tardes, ya cree una clase

---
+++ src/co/edu/unbosque/fourpawscitizens/model/Pet.java
@@ -0,0 +1,110 @@
+package co.edu.unbosque.fourpawscitizens.model;
+
+public class Pet {
+	String id;
+	long microChip;
+	String species;
+	String sex;
+	String size;
+	boolean potentDangerous;
+	String neighborhood;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the microChip
+	 */
+	public long getMicroChip() {
+		return microChip;
+	}
+
+	/**
+	 * @param microChip the microChip to set
+	 */
+	public void setMicroChip(long microChip) {
+		this.microChip = microChip;
+	}
+
+	/**
+	 * @return the species
+	 */
+	public String getSpecies() {
+		return species;
+	}
+
+	/**
+	 * @param species the species to set
+	 */
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	/**
+	 * @return the sex
+	 */
+	public String getSex() {
+		return sex;
+	}
+
+	/**
+	 * @param sex the sex to set
+	 */
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public String getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	/**
+	 * @return the potentDangerous
+	 */
+	public boolean isPotentDangerous() {
+		return potentDangerous;
+	}
+
+	/**
+	 * @param potentDangerous the potentDangerous to set
+	 */
+	public void setPotentDangerous(boolean potentDangerous) {
+		this.potentDangerous = potentDangerous;
+	}
+
+	/**
+	 * @return the neighborhood
+	 */
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	/**
+	 * @param neighborhood the neighborhood to set
+	 */
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+}
